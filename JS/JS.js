1942648daafd900f1c6545aa73baec40dbf3d0b8
AnnaLevitski <annalevitskiqa@gmail.com>
2023-02-09 19:09:39 +0200

    fixed bug with signs

--- JS/JS.js
+++ JS/JS.js
@@ -2,72 +2,49 @@ let state = 0;   // condition 0 - first num 1 - second num  2 - get and print re
 let a = [];      // first num storage
 let b = [];      // second num storage
 let tRes = [];   // resolt num storage  
-let act;         // action button value storage
+let act = 0;         // action button value storage
+let queueAct = [];
+const enqueue = (item) => queueAct.push(item);  // add to the end queueAct[]
+let queueCount = 0;
 
 //action buttons from buttons 
 function getButton(tap){ 
-    switch (tap) {
+    if(tap != ('point' && 'equal')){
+        queueCount++;
+        console.log('queueCount ' + queueCount);
+        enqueue(tap);
+        console.log(queueAct);
+        console.log('state ' + state);
+        if(state !=0){
+            console.log('here');
+
+            act = queueAct[queueCount-2];
+        }
+        else{
+            act = tap;
+        }
+        console.log(act);
+    }
+    switch (act) {
         case 'sum':
-            if(state == 2){
-                state = 3;
-                act = tap;
-                console.log(act);
-                continueAfterEqals();
-                break;
-            };
-            state = 1; 
-            act = tap;
-            console.log(act);
-            break;
         case 'min':
-            if(state == 2){
-                state = 3;
-                act = tap;
-                console.log(act);
-                continueAfterEqals();
-                break;
-            };
-            act = tap;
-            state = 1;
-            console.log(act); 
-            break;
         case 'mult':
-            if(state == 2){
-                state = 3;
-                act = tap;
-                console.log(act);
-                continueAfterEqals();
-                break;
-            };
-            act = tap;
-            state = 1; 
-            console.log(act);
-            break;
         case 'div':
-            if(state == 2){
-                state = 3;
-                act = tap;
-                console.log(act);
-                continueAfterEqals();
+            if(state != 0 ){
+                //act = tap; 
+                equal();
+                continueAfterEqals(); 
                 break;
             };
-            act = tap;
             state = 1; 
-            console.log(act);
+            //act = tap;
             break;
         case 'point':  
-            console.log(tap);
             getNum('.');
             break;
         case 'equal':
-            console.log('getButton' + state);
-            a = Array.isArray(a) ? Number(a.join('')) : Number(a);
-            console.log(a, typeof(a));
-            b = Number(b.join(''));
-            console.log(b, typeof(b));
-            ifEqual(act, a, b);
+            equal();
             state = 2; 
-            console.log('getButton' + state);
             break;
         default :
         console.log('NaN tap');
@@ -75,31 +52,32 @@ function getButton(tap){
     }
 };
 
+// run ifEqual 
+function equal(){
+    a = Array.isArray(a) ? Number(a.join('')) : Number(a);
+    b = (Array.isArray(b)) ? Number(b.join('')) : Number(b);
+    console.log(a, act, b);
+    ifEqual(act, a, b);
+    state = 2; 
+}
+
 //get nuber buttons from buttons 
 function getNum(num){
     switch (state) {
         case 0:
+        case 2:
             a.push(num);
-            console.log(a, typeof(a), 'A'); 
+            //console.log(a, typeof(a), 'getNum A'); 
+           // console.log(b, typeof(b), 'getNum B'); 
             printOnScreen(a);
             break;
         case 1:
-            b.push(num);
-            console.log(b, typeof(b), 'B'); 
-            printOnScreen(b);
-            break;
-        case 2:
-            clean(); 
-            a.push(num);
-            console.log(a, typeof(a), 'A'); 
-            printOnScreen(a);
-            break;
         case 3:
-            clean();
             b.push(num);
-            console.log(b, typeof(b), 'B'); 
+           // console.log(a, typeof(a), 'getNum A'); 
+           // console.log(b, typeof(b), 'getNum B'); 
             printOnScreen(b);
-            break;   
+            break; 
         default :
             console.log('NaN state');
             break;
@@ -112,12 +90,12 @@ function ifEqual(act, a, b){
     switch (act) {
         case 'sum':
             res = a + b;
-            console.log(res);
+            console.log(Number(res.toFixed(4)));
             tRes = res;
             printOnScreen(Number(res.toFixed(4)));
             break;
         case 'min':
-            res = a - b;
+            res = (a) - (b);
             console.log(res);
             tRes = res;
             printOnScreen(Number(res.toFixed(4)));
@@ -143,14 +121,14 @@ function ifEqual(act, a, b){
 function continueAfterEqals(){
     clean();
     a = tRes;
-    state = 1;
+    state = 3;
 };
 
 // clean vars 
 function clean(){
-    state = 0;  
-    a = []; 
-    b = []; 
+        state = 0;  
+        a = []; 
+        b = []; 
 };
 
 //print on the scrin 
@@ -161,7 +139,6 @@ function printOnScreen(a){
 }
 
 
-// добавить возможность продолжать считать после  if (tap) a = res;  else clean(); 
 
 
 
